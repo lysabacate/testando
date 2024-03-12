ee3c49af385d66b3239a4792492db8449faee634
lysabacate <133353329+lysabacate@users.noreply.github.com>
2024-03-12 14:11:20 +0000

    testando aula imagem

--- app.py
+++ app.py
@@ -36,3 +36,9 @@ def gastos(mes, valor):
 def dobro(n):
     resultado = 2*n
     return render_template('dobro.html', n=n, resultado = resultado)
+
+
+@app.route('/perfil', defaults= {'nome':'anonimo'})
+@app.route('/perfil/<nome>')
+def perfil(nome):
+    return render_template('perfil.html', nome = nome)
